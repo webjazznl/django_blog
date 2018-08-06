c5a09aa015579469accf564c08e40506e4b7b9e1
webjazznl <hsiebelink73@gmail.com>
2018-08-06 13:13:23 +0200

    Add post detail url, view and template

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name="blog"
+
 urlpatterns = [
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+# ignore error: %s %r has no %r member 
+# pylint: disable = E1101 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    post_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'post_list': post_list})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk = post_id)
+    return render(request,'blog/post_detail.html', {'post': post})
